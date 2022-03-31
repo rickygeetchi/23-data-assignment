c218606dfc8e32a0c08d7207372e216e213d6225
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-03-30 20:21:40 -0500

    This is my data assignment solution

---
+++ README.md
@@ -0,0 +1,1 @@
+My solution for 2.3.
---
+++ src/main.js
@@ -0,0 +1,11 @@
+const greeting = "Hello class, my name is Roderick.";
+
+console.log(greeting);
+
+const age = 26;
+
+console.log("Age: " + age);
+
+const likesColdWeather = true;
+
+console.log("Likes cold weather: " + likesColdWeather);
